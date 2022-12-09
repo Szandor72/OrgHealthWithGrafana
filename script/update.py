3e327df7f300d4d1c9d21dc6461474b40a9b85ca
Szandor72 <szandor72@gmail.com>
2022-12-09 15:33:10 +0100

    feat: updates to login and org shape

--- script/update.py
+++ script/update.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 
+from dotenv import load_dotenv
 from simple_salesforce import Salesforce
 from base64 import b64decode as b
 import requests
@@ -17,6 +18,9 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 # Variables
 # ==========================================
 
+
+load_dotenv()
+
 SF_CONSUMER_KEY = os.environ.get('SF_CONSUMER_KEY')
 SF_CONSUMER_SECRET = os.environ.get('SF_CONSUMER_SECRET')
 SF_USERNAME = os.environ.get('SF_USERNAME')
@@ -29,42 +33,41 @@ INF_ORG = os.environ.get('INF_ORG')
 INF_BUCKET = os.environ.get('INF_BUCKET')
 record_to_insert = []
 
-# ========================================== 
+# ==========================================
 # Init
 # ==========================================
 
 """
-Connect to salesforce VIA SImple_Salesforce
-"""
-sf = Salesforce(username=SF_USERNAME,password=SF_PASSWORD, security_token=SF_TOKEN)
-
-"""
-Connect to salesforce via API
+Connect to salesforce via API and then Simple_Salesforce
 """
 
 
+sfdcUrl = "https://login.salesforce.com/services/oauth2/token"
 params = {
     "grant_type": "password",
-    "client_id": SF_CONSUMER_KEY, # Consumer Key
-    "client_secret": SF_CONSUMER_SECRET, # Consumer Secret
-    "username": SF_USERNAME, # The email you use to login
-    "password": SF_PASSWORD+SF_TOKEN # Concat your password and your security token 
+    "client_id": SF_CONSUMER_KEY,  # Consumer Key
+    "client_secret": SF_CONSUMER_SECRET,  # Consumer Secret
+    "username": SF_USERNAME,  # The email you use to login
+    "password": SF_PASSWORD+SF_TOKEN  # Concat your password and your security token
 }
-# if you connect to a Sandbox, use test.salesforce.com instead
-r = requests.post("https://login.salesforce.com/services/oauth2/token", params=params)
+r = requests.post(
+    sfdcUrl, params=params)
 access_token = r.json().get("access_token")
 instance_url = r.json().get("instance_url")
 print("r: ", r.json())
 print("Access Token:", access_token)
 print("Instance URL", instance_url)
 
+sf = Salesforce(instance_url=instance_url,
+                session_id=access_token,
+                version="55.0")
 
 
 # ==========================================
 # Utilities
 # ==========================================
 
-def sf_api_call(action, parameters = {}, method = 'get', data = {}):
+def sf_api_call(action, parameters={}, method='get', data={}):
     """
     Helper function to make calls to Salesforce REST API.
     Parameters: action (the URL), URL params, method (get, post or patch), data for POST/PATCH.
@@ -75,15 +78,17 @@ def sf_api_call(action, parameters = {}, method = 'get', data = {}):
         'Authorization': 'Bearer %s' % access_token
     }
     if method == 'get':
-        r = requests.request(method, instance_url+action, headers=headers, params=parameters, timeout=30)
+        r = requests.request(method, instance_url+action,
+                             headers=headers, params=parameters, timeout=30)
     elif method in ['post', 'patch']:
-        r = requests.request(method, instance_url+action, headers=headers, json=data, params=parameters, timeout=10)
+        r = requests.request(method, instance_url+action,
+                             headers=headers, json=data, params=parameters, timeout=10)
     else:
         # other methods not implemented in this example
         raise ValueError('Method should be get or post or patch.')
-    print('Debug: API %s call: %s' % (method, r.url) )
+    print('Debug: API %s call: %s' % (method, r.url))
     if r.status_code < 300:
-        if method=='patch':
+        if method == 'patch':
             return None
         else:
             return r.json()
@@ -94,15 +99,20 @@ def sf_api_call(action, parameters = {}, method = 'get', data = {}):
 """
 Timestamp
 """
+
+
 def insertTimestamp():
 
-    _pointa = Point("log").tag("type", "datetime").field("last update", datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+    _pointa = Point("log").tag("type", "datetime").field(
+        "last update", datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
     record_to_insert.append(_pointa)
 
 
 """
 Get Salesforce limits
 """
+
+
 def getsalesforcelimits():
 
     result = sf.query_more("/services/data/v50.0/limits", True)
@@ -114,8 +124,8 @@ def getsalesforcelimits():
             used_percent = (count / result[element]['Max']) * 100
 
         record_to_insert.append({
-            "measurement": "limits", 
-            "tags": {"type": element}, 
+            "measurement": "limits",
+            "tags": {"type": element},
             "fields": {
                 "Done": count,
                 "Remaining": result[element]['Remaining'],
@@ -125,20 +135,24 @@ def getsalesforcelimits():
         })
 
 
-
 """
 Get Salesforce Licenses
 """
+
+
 def getsalesforcelicense():
 
-    result2 = sf.query_all("select Id, Name, UsedLicenses, TotalLicenses, Status, MasterLabel, LicenseDefinitionKey from UserLicense WHERE Name ='Salesforce'")
-    remains = result2['records'][0]['TotalLicenses'] - result2['records'][0]['UsedLicenses']
+    result2 = sf.query_all(
+        "select Id, Name, UsedLicenses, TotalLicenses, Status, MasterLabel, LicenseDefinitionKey from UserLicense WHERE Name ='Salesforce'")
+    remains = result2['records'][0]['TotalLicenses'] - \
+        result2['records'][0]['UsedLicenses']
     if (result2['records'][0]['TotalLicenses'] != 0):
-        percent_used = (result2['records'][0]['UsedLicenses'] / result2['records'][0]['TotalLicenses']) * 100
-        
+        percent_used = (result2['records'][0]['UsedLicenses'] /
+                        result2['records'][0]['TotalLicenses']) * 100
+
     record_to_insert.append({
-        "measurement": "license", 
-        "tags": {"type": "Licenses"}, 
+        "measurement": "license",
+        "tags": {"type": "Licenses"},
         "fields": {
             "Used": result2['records'][0]['UsedLicenses'],
             "Remains": remains,
@@ -151,41 +165,50 @@ def getsalesforcelicense():
 """
 Get Salesforce count records
 """
+
+
 def countRecords():
 
     result = sf.query_more("/services/data/v50.0/limits/recordCount", True)
     for element in result['sObjects']:
 
         record_to_insert.append({
-            "measurement": "CountRecords", 
-            "tags": {"type": element['name']}, 
+            "measurement": "CountRecords",
+            "tags": {"type": element['name']},
             "fields": {
                 "counts": element['count'],
                 "size": element['count'] * 2
             }
         })
 
+
 """
 Get Last Logs
 """
+
+
 def getSalesforceApexlog():
 
-    result = json.dumps(sf_api_call('/services/data/v53.0/tooling/query/?q=SELECT+Request,+LogUserId,+Status,+DurationMilliseconds,+SystemModstamp+from+ApexLog'), indent=2)
-    
+    result = json.dumps(sf_api_call(
+        '/services/data/v53.0/tooling/query/?q=SELECT+Request,+LogUserId,+Status,+DurationMilliseconds,+SystemModstamp+from+ApexLog'), indent=2)
+
 
 """
 Get Salesforce Instance info
 """
+
+
 def getsalesforceInstance():
 
     result2 = sf.query_all("Select FIELDS(ALL) From Organization LIMIT 1")
     pod = result2['records'][0]['InstanceName']
 
-    responsestatus = requests.get("https://api.status.salesforce.com/v1/instances/"+ pod +"/status/preview?childProducts=false")
+    responsestatus = requests.get(
+        "https://api.status.salesforce.com/v1/instances/" + pod + "/status/preview?childProducts=false")
 
     record_to_insert.append({
-        "measurement": "OrgInformation", 
-        "tags": {"type": 'OrgInformation'}, 
+        "measurement": "OrgInformation",
+        "tags": {"type": 'OrgInformation'},
         "fields": {
             "Pod": result2['records'][0]['InstanceName'],
             "OrganizationType": result2['records'][0]['OrganizationType'],
@@ -202,10 +225,12 @@ def getsalesforceInstance():
 """
 Get Salesforce Incident
 """
+
+
 def getsalesforceIncidents(instancepod):
 
-    response = requests.get("https://api.status.salesforce.com/v1/incidents/active")
-
+    response = requests.get(
+        "https://api.status.salesforce.com/v1/incidents/active")
 
     incident_to_insert = []
     no_incident_to_insert = ''
@@ -214,71 +239,70 @@ def getsalesforceIncidents(instancepod):
     severity = ''
 
     for element in response:
-        try: 
+        try:
             message = response.json()[element]['IncidentEvents'][0]['message']
             endTime = response.json()[element]['IncidentImpacts'][0]['endTime']
             severity = response.json()[element]['severity']
         except:
             pass
 
-        try: 
+        try:
             pod = response.json()[element]['instanceKeys']
             pod = str(pod)
             pod = pod.replace("'", "").replace("[", "").replace("]", "")
         except:
             pass
         #print(instancepod + ' vs ' + pod)
-        if (instancepod  in pod) :
+        if (instancepod in pod):
             incident = {
-            "measurement": "SF_Incident", 
-            "tags": { "Pod": pod, 
-                },
-            "fields": {
-                "id": '',
-                "IsError": 20,
-                "message": message,
-                "severity": severity
-                    }
+                "measurement": "SF_Incident",
+                "tags": {"Pod": pod,
+                         },
+                "fields": {
+                    "id": '',
+                    "IsError": 20,
+                    "message": message,
+                    "severity": severity
+                }
             }
             incident_to_insert.append(incident)
-            print(incident) 
+            print(incident)
         else:
             incident = {
-            "measurement": "SF_Incident", 
-            "tags": { "Pod": pod, 
-                },
-            "fields": {
-                "id": '',
-                "IsError": 10,
-                "message": "Pod" + pod + " OK"
-                    }
+                "measurement": "SF_Incident",
+                "tags": {"Pod": pod,
+                         },
+                "fields": {
+                    "id": '',
+                    "IsError": 10,
+                    "message": "Pod" + pod + " OK"
+                }
             }
             no_incident_to_insert = incident
 
-        if ( len(incident_to_insert) == 0):
+        if (len(incident_to_insert) == 0):
             record_to_insert.append(no_incident_to_insert)
         else:
             record_to_insert.append(incident_to_insert)
 
 
-
-
 """
 Update Database
 """
+
+
 def update_database_sync():
     client = InfluxDBClient(url=INF_URL, token=INF_TOKEN, org=INF_ORG)
     write_api = client.write_api(write_options=SYNCHRONOUS)
     getsalesforcelimits()
     getsalesforcelicense()
     instancepod = getsalesforceInstance()
-    getsalesforceIncidents(instancepod) 
+    getsalesforceIncidents(instancepod)
     countRecords()
     insertTimestamp()
     getSalesforceApexlog()
     print(record_to_insert)
     write_api.write(bucket=INF_BUCKET, record=record_to_insert)
-    
 
 
 update_database_sync()
--- script/update_hourly.py
+++ script/update_hourly.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 
+from dotenv import load_dotenv
 from simple_salesforce import Salesforce
 from base64 import b64decode as b
 import requests
@@ -17,6 +18,8 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 # Variables
 # ==========================================
 
+load_dotenv()
+
 SF_CONSUMER_KEY = os.environ.get('SF_CONSUMER_KEY')
 SF_CONSUMER_SECRET = os.environ.get('SF_CONSUMER_SECRET')
 SF_USERNAME = os.environ.get('SF_USERNAME')
@@ -29,40 +32,41 @@ INF_ORG = os.environ.get('INF_ORG')
 INF_BUCKET = os.environ.get('INF_BUCKET')
 record_to_insert = []
 
-# ========================================== 
+# ==========================================
 # Init
 # ==========================================
 
 """
-Connect to salesforce
-"""
-sf = Salesforce(username=SF_USERNAME,password=SF_PASSWORD, security_token=SF_TOKEN)
-
-"""
-Connect to salesforce via API
+Connect to salesforce via API and then Simple_Salesforce
 """
 
+
+sfdcUrl = "https://login.salesforce.com/services/oauth2/token"
 params = {
     "grant_type": "password",
-    "client_id": SF_CONSUMER_KEY, # Consumer Key
-    "client_secret": SF_CONSUMER_SECRET, # Consumer Secret
-    "username": SF_USERNAME, # The email you use to login
-    "password": SF_PASSWORD+SF_TOKEN # Concat your password and your security token
+    "client_id": SF_CONSUMER_KEY,  # Consumer Key
+    "client_secret": SF_CONSUMER_SECRET,  # Consumer Secret
+    "username": SF_USERNAME,  # The email you use to login
+    "password": SF_PASSWORD+SF_TOKEN  # Concat your password and your security token
 }
-# if you connect to a Sandbox, use test.salesforce.com instead
-r = requests.post("https://login.salesforce.com/services/oauth2/token", params=params)
+r = requests.post(
+    sfdcUrl, params=params)
 access_token = r.json().get("access_token")
 instance_url = r.json().get("instance_url")
 print("r: ", r.json())
 print("Access Token:", access_token)
 print("Instance URL", instance_url)
 
+sf = Salesforce(instance_url=instance_url,
+                session_id=access_token,
+                version="55.0")
+
 
 # ==========================================
 # Utilities
 # ==========================================
 
-def sf_api_call(action, parameters = {}, method = 'get', data = {}):
+def sf_api_call(action, parameters={}, method='get', data={}):
     """
     Helper function to make calls to Salesforce REST API.
     Parameters: action (the URL), URL params, method (get, post or patch), data for POST/PATCH.
@@ -73,15 +77,17 @@ def sf_api_call(action, parameters = {}, method = 'get', data = {}):
         'Authorization': 'Bearer %s' % access_token
     }
     if method == 'get':
-        r = requests.request(method, instance_url+action, headers=headers, params=parameters, timeout=30)
+        r = requests.request(method, instance_url+action,
+                             headers=headers, params=parameters, timeout=30)
     elif method in ['post', 'patch']:
-        r = requests.request(method, instance_url+action, headers=headers, json=data, params=parameters, timeout=10)
+        r = requests.request(method, instance_url+action,
+                             headers=headers, json=data, params=parameters, timeout=10)
     else:
         # other methods not implemented in this example
         raise ValueError('Method should be get or post or patch.')
-    print('Debug: API %s call: %s' % (method, r.url) )
+    print('Debug: API %s call: %s' % (method, r.url))
     if r.status_code < 300:
-        if method=='patch':
+        if method == 'patch':
             return None
         else:
             return r.json()
@@ -89,101 +95,103 @@ def sf_api_call(action, parameters = {}, method = 'get', data = {}):
         raise Exception('API error when calling %s : %s' % (r.url, r.content))
 
 
-
 def login_log():
     result = sf.query_all("SELECT id, ApiType, ApiVersion, Application, AuthenticationServiceId, AuthMethodReference, Browser, ClientVersion, CountryIso,  \
-                            LoginGeo.City, LoginGeo.Longitude, LoginGeo.Latitude, SourceIp, Status, NetworkId, LoginTime, Platform, UserId  \
+                            LoginGeo.City, LoginGeo.Longitude, LoginGeo.Latitude, SourceIp, Status, LoginTime, Platform, UserId  \
                             FROM LoginHistory \
                             WHERE LoginTime = TODAY \
                             AND HOUR_IN_DAY(LoginTime) > 2")
-    
-    for element in result['records']:          
-            print(element['LoginGeo']['Longitude'])
-            print(element['LoginGeo']['Latitude'])
-            print(element['LoginGeo']['City'])
-            print()
-            print(element['UserId'])
-            print(element['SourceIp'])
-       
-            point = {
-                "measurement": "logins.logs",
-                "tags": {
+
+    for element in result['records']:
+        print(element['LoginGeo']['Longitude'])
+        print(element['LoginGeo']['Latitude'])
+        print(element['LoginGeo']['City'])
+        print()
+        print(element['UserId'])
+        print(element['SourceIp'])
+
+        point = {
+            "measurement": "logins.logs",
+            "tags": {
                 "geohash": pgh.encode(latitude=element['LoginGeo']['Latitude'], longitude=element['LoginGeo']['Longitude'])
-                },
-                "time": element['LoginTime'],
-                "fields": {
-                    "Longitude": element['LoginGeo']['Longitude'],
-                    "Latitude": element['LoginGeo']['Latitude'],
-                    "Geohash": pgh.encode(latitude=element['LoginGeo']['Latitude'], longitude=element['LoginGeo']['Longitude']),
-                    "Country": element['CountryIso'],
-                    "City": element['LoginGeo']['City'],
-                    "Status": element['Status'],
-                    "Ip": element['SourceIp'],
-                    "Application": element['Application'],
-                    "NetworkId": element['NetworkId'],
-                    "AuthMethodReference": element['AuthMethodReference'],
-                    "UserId": element['UserId'],
-                    "Platform": element['Platform'],
-                    "AuthenticationServiceId": element['AuthenticationServiceId']
+            },
+            "time": element['LoginTime'],
+            "fields": {
+                "Longitude": element['LoginGeo']['Longitude'],
+                "Latitude": element['LoginGeo']['Latitude'],
+                "Geohash": pgh.encode(latitude=element['LoginGeo']['Latitude'], longitude=element['LoginGeo']['Longitude']),
+                "Country": element['CountryIso'],
+                "City": element['LoginGeo']['City'],
+                "Status": element['Status'],
+                "Ip": element['SourceIp'],
+                "Application": element['Application'],
+                "AuthMethodReference": element['AuthMethodReference'],
+                "UserId": element['UserId'],
+                "Platform": element['Platform'],
+                "AuthenticationServiceId": element['AuthenticationServiceId']
 
 
-                    }
             }
-            print(point)
-            record_to_insert.append(point)
+        }
+        print(point)
+        record_to_insert.append(point)
 
 
 """
 Update get SecurityHealthCheck
 """
+
+
 def get_SecurityHealthCheck():
-    score = sf_api_call('/services/data/v53.0/tooling/query/?q=SELECT+Score+FROM+SecurityHealthCheck')
+    score = sf_api_call(
+        '/services/data/v53.0/tooling/query/?q=SELECT+Score+FROM+SecurityHealthCheck')
     score['records'][0]['Score']
     point = {
-                "measurement": "SecurityHealthCheckScore",
-                "tags": {
-                "type": 'Security'
-                },
-                "fields": {
-                    "Score": score['records'][0]['Score']
-                    }
-            }
+        "measurement": "SecurityHealthCheckScore",
+        "tags": {
+            "type": 'Security'
+        },
+        "fields": {
+            "Score": score['records'][0]['Score']
+        }
+    }
     record_to_insert.append(point)
 
 
 """
 get SecurityHealthCheck RISk
 """
+
+
 def get_SecurityHealthCheckRisks():
-    i=0
-    response = sf_api_call('/services/data/v53.0/tooling/query/?q=SELECT+RiskType,+Setting,+SettingGroup,+OrgValue,+StandardValue+FROM+SecurityHealthCheckRisks+where+RiskType=\'HIGH_RISK\'+OR+RiskType=\'MEDIUM_RISK\'')
+    i = 0
+    response = sf_api_call(
+        '/services/data/v53.0/tooling/query/?q=SELECT+RiskType,+Setting,+SettingGroup,+OrgValue,+StandardValue+FROM+SecurityHealthCheckRisks+where+RiskType=\'HIGH_RISK\'+OR+RiskType=\'MEDIUM_RISK\'')
     for element in response['records']:
         record_to_insert.append({
-                "measurement": "SecurityHealthCheck",
-                "tags": {
+            "measurement": "SecurityHealthCheck",
+            "tags": {
                 "type": element['Setting'],
                 "detail": element['SettingGroup'],
                 "increment": i
-                },
-                "time": datetime.now() + timedelta(seconds=i),
-                "fields": {
-                    "RiskType": element['RiskType'],
-                    "Setting": element['Setting'],
-                    "OrgValue": element['OrgValue'],
-                    "StandardValue": element['StandardValue'],
-                    "SettingGroup": element['SettingGroup']
-                    }
+            },
+            "time": datetime.now() + timedelta(seconds=i),
+            "fields": {
+                "RiskType": element['RiskType'],
+                "Setting": element['Setting'],
+                "OrgValue": element['OrgValue'],
+                "StandardValue": element['StandardValue'],
+                "SettingGroup": element['SettingGroup']
+            }
         })
         i = i + 1
-
-
-
-
 
 
 """
 Update Database
 """
+
+
 def update_database_sync():
     client = InfluxDBClient(url=INF_URL, token=INF_TOKEN, org=INF_ORG)
     write_api = client.write_api(write_options=SYNCHRONOUS)
@@ -192,6 +200,6 @@ def update_database_sync():
     get_SecurityHealthCheckRisks()
     print(record_to_insert)
     write_api.write(bucket=INF_BUCKET, record=record_to_insert)
-    
+
 
 update_database_sync()
